5657eb642433c4370aec3065bb5dc6be86f10dda
Manel Martinez Bel <manelmb9@gmail.com>
2026-01-22 08:09:30 +0100

    Implement name concatenation and capitalization

--- prueba-js/src/concatenar.js
+++ prueba-js/src/concatenar.js
@@ -1,5 +1,17 @@
 export default (json) => {
-    // Do your thing here!
-    return "";
-  };
-  
+  const capitalize = (text) =>
+    (text || "")
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ");
+
+  return json
+    .map((person) => {
+      const fullName = [capitalize(person.nombre), capitalize(person.apellido)];
+      if (person.apellido2) fullName.push(capitalize(person.apellido2));
+      return fullName.join(" ") + ".";
+    })
+    .join("<br>");
+};
