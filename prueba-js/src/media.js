e411e9670b327ef16b3e0f35c4df43131fb494d4
Manel Martinez Bel <manelmb9@gmail.com>
2026-01-22 08:13:15 +0100

    Compute average age from JSON

--- prueba-js/src/media.js
+++ prueba-js/src/media.js
@@ -1,5 +1,11 @@
 export default (json) => {
-    // Do your thing here!
-    return "";
-  };
-  
+  let sum = 0;
+
+  for (const person of json) {
+    sum += person.edad;
+  }
+
+  const avg = sum / json.length;
+
+  return Number(avg.toFixed(2)).toString().replace('.', ',');
+};
