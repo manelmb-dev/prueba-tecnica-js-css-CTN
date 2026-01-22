0b12f92aa37f5b121b83f66561f3fb0ba28d073c
Manel Martinez Bel <manelmb9@gmail.com>
2026-01-22 08:20:08 +0100

    Implement anagram check (ignore case, accents and punctuation)

--- prueba-js/src/anagrama.js
+++ prueba-js/src/anagrama.js
@@ -1,5 +1,15 @@
 export default (word1, word2) => {
-    // Do your thing here!
-    return false;
-  };
-  
+  const formatWord = (word) =>
+    (word || "")
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[\W_]/g, "")
+      .split("")
+      .sort()
+      .join("");
+
+  return formatWord(word1) === formatWord(word2);
+};
+
+// Entiendo que los casos en los que hay signos (por ejemplo, una coma) como "[Anagrama]" y "[Amar, gana]" también deben considerarse anagramas.
